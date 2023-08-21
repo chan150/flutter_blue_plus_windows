04e2cbdd0c116a37ea0869b17a27a98443fb9633
Himchan Park <chan150@kaist.ac.kr>
2023-08-21 10:30:27 +0900

    flutter_blue_plus_windows

--- lib/flutter_blue_plus_windows.dart
+++ lib/flutter_blue_plus_windows.dart
@@ -1,4 +1,2 @@
-/// Flutter blue plus for Windows
 library flutter_blue_plus_windows;
 
-export 'src/flutter_blue_plus_windows.dart';
--- lib/src/flutter_blue_plus_windows.dart
+++
@@ -1,7 +0,0 @@
-/// {@template flutter_blue_plus_windows}
-/// Flutter blue plus for Windows
-/// {@endtemplate}
-class FlutterBluePlusWindows {
-  /// {@macro flutter_blue_plus_windows}
-  const FlutterBluePlusWindows();
-}
