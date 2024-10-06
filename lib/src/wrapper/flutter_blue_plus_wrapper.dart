5544ff5acf0742aaac8e9f6499f5933eb49af4de
Jeff <jeff.a.longo@gmail.com>
2024-10-06 14:12:04 -0700

    Update flutter_blue_plus_wrapper.dart
    Fix build with FBP 1.33.x

--- lib/src/wrapper/flutter_blue_plus_wrapper.dart
+++ lib/src/wrapper/flutter_blue_plus_wrapper.dart
@@ -113,9 +113,9 @@ class FlutterBluePlus {
     return Mobile.FlutterBluePlus.connectedDevices;
   }
 
-  static Future<List<BluetoothDevice>> get systemDevices async {
+  static Future<List<BluetoothDevice>> systemDevices(List<Guid> withServices) async {
     if (Platform.isWindows) return FlutterBluePlusWindows.connectedDevices;
-    return await Mobile.FlutterBluePlus.systemDevices;
+    return await Mobile.FlutterBluePlus.systemDevices(withServices);
   }
 
   static Future<PhySupport> getPhySupport() {
